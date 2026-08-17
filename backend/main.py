be96cab793c82fa92137b67a64f95fedb9f75a5e
Gottam Sairam <sairangottam810@gmail.com>
2026-08-17 12:00:38 +0530

    Fix CORS

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ allowed_origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:5173").split(",
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
